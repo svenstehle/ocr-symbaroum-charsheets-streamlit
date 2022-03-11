a9bcd842fafce24c78cb1e5fbd54574dbe618b4f
stehles <sven.stehle@boehringer-ingelheim.com>
2022-03-12 00:31:00 +0100

    working draft for streamlit with many display features, pruning now

---
+++ README.md
@@ -0,0 +1,20 @@
+# Symbaroum Initiative Tracker
+
+This is an initiative tracker that was created with the RPG Symbaroum in mind.
+It can be adjusted and used for any RPG game.
+
+## Quickstart
+
+To have an easy start you just need to install streamlit and pandas.
+
+```bash
+pip install pandas streamlit
+```
+
+And then run the streamlit app with
+
+```bash
+streamlit run initiative.py
+```
+
+Have fun and raise any issues you see. Feel free to contribute and extend.
--- initiative.py
+++ initiative.py
@@ -1,18 +1,36 @@
-import pandas as pd
 import sys
 
-def get_all_characters():
-    player_chars = {"Dalai": 13, "Tower": 10, "Eagle": 14, "Vatras": 8}
-    npcs = {"zmob1": 13, "zmob2": 13, "zmob3": 13}
+import pandas as pd
+import streamlit as st
 
-    combined_chars = {**player_chars, **npcs}
-    combined_chars = pd.Series(combined_chars)
-    combined_chars = combined_chars.reset_index()
-    combined_chars.rename(columns={"index": "name", 0: "initiative"}, inplace=True)
-    combined_chars.sort_values(by="initiative", inplace=True, ascending=False)
-    combined_chars.reset_index(drop=True, inplace=True)
-    combined_chars["name"] = combined_chars["name"].str.lower()
-    return combined_chars
+class Characters:
+    def __init__(self):
+        self.player_chars = {"Dalai": 13, "Tower": 10, "Eagle": 14, "Vatras": 8}
+        self.npcs = {"zmob1": 13, "zmob2": 13, "zmob3": 13}
+
+    def get_combined_chars(self):
+        combined_chars = {**self.player_chars, **self.npcs}
+        combined_chars = pd.Series(combined_chars)
+        combined_chars = combined_chars.reset_index()
+        combined_chars.rename(columns={"index": "name", 0: "initiative"}, inplace=True)
+        combined_chars.sort_values(by="initiative", inplace=True, ascending=False)
+        combined_chars.reset_index(drop=True, inplace=True)
+        combined_chars["name"] = combined_chars["name"].str.lower()
+        self.combined_characters = combined_chars
+        return combined_chars
+
+    def get_player_char_names(self):
+        return pd.Series(self.player_chars.keys()).str.lower()
+
+    def get_name_initiative_tuples(self):
+        # all_names_inits = (self.combined_characters["name"] + ": " + str(self.combined_characters["initiative"])).values
+        player_names_inits = [f"{k.lower()}: {v}" for k, v in self.player_chars.items()]
+        # st.write(all_names_inits)
+        # return all_names_inits, player_names_inits
+        return player_names_inits, player_names_inits
+
+
+
 
 def get_next_char_name(combined_chars, current):
     index_current = combined_chars.loc[combined_chars["name"]==current].index.values[0]
@@ -26,25 +44,33 @@ def get_next_char_name(combined_chars, current):
     return next_char
 
 def display_turn_of_next_char():
-    combined_chars = get_all_characters()
+    all_chars = Characters()
+    combined_chars = all_chars.get_combined_chars()
     names = list(combined_chars["name"])
+    st.write("Try out streamlit for our initiative tracker")
+    st.write("List of all characters:")
 
-    while True:
-        current = input("\nType in current char or NPC name. Available are {}. \nType 'exit' if you want to end: ".format(names)).lower()
+    st.write(combined_chars)
+    st.write(f"player char names: {all_chars.get_player_char_names()}")
+    st.write("test")
 
-        if current == "exit":
-            sys.exit("Exiting")
-        elif current not in names:
-            print("\n Your selection ***\t{}\t*** is not available. Please select a name that is available for selection.".format(current))
-            continue
+    all_available, players = all_chars.get_name_initiative_tuples()
+    chars_to_consider = st.multiselect(
+        'Select which characters are alive and in combat so we can consider their initiative',
+        all_available,
+        players,
+    )
+    names_of_chars = [x.split(":")[0].lower() for x in chars_to_consider]
+    st.write('You want to track initiative for:')
+    names_and_initiatives = combined_chars[combined_chars["name"].isin(names_of_chars)]
+    st.write(names_and_initiatives)
 
-        character_list = "\nList of all characters: \n{}\n".format(combined_chars)
-        print(character_list)
+    selected = st.sidebar.selectbox("Select character for current turn".upper(), names_and_initiatives)
+    st.write(f"Current selected character is '_{selected}_'")
 
-        next_char = get_next_char_name(combined_chars, current)
-        result = "The character ***\t{}\t*** has the next turn".format(next_char.upper())
-        print(result)
-
+    next_char = get_next_char_name(combined_chars, selected)
+    result = "The character \t**{}**\t has the next turn".format(next_char.upper())
+    st.write(result)
 
 def main():
     display_turn_of_next_char()
