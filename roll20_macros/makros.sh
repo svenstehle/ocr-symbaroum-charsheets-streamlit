8ce7db24ee2bf4fc0b6185e681363a8da1a53791
stehles <sven.stehle@boehringer-ingelheim.com>
2022-04-17 13:49:24 +0200

    move adventure notes to separate repo

--- roll20_macros/makros.sh
+++
@@ -1,9 +0,0 @@
-#attack:
-/me attacks and rolls vs strong(modified)=[[1d20ro<[[@{(Sven) Dalai Bhikschu Kamehame|strong}  + ?{Modifier} ]] ]] / [[@{(Sven) Dalai Bhikschu Kamehame|strong}  + ?{Modifier} ]]
-
-#damage:
-/me IF HIT: inflicting [[1 + @{(Sven) Dalai Bhikschu Kamehame|weapon1-dtype} + @{(Sven) Dalai Bhikschu Kamehame|weapon1-extra-dtype} ]] damage.
-
-#hit-combi:
-#attack
-#damage
