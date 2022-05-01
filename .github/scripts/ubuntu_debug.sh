916532ece58e8fa67f09209808cb4b190bb8df44
stehles <svenstehle@web.de>
2022-05-01 18:59:56 +0200

    make all macos scripts executable and change manner of execution in CI

--- .github/scripts/ubuntu_debug.sh
+++ .github/scripts/ubuntu_debug.sh
@@ -7,3 +7,7 @@ sudo apt update &&
     wget https://github.com/tesseract-ocr/tessdata_best/raw/main/deu.traineddata &&
     export TESSDATA_PREFIX=/usr/local/share/tessdata &&
     tesseract --list-langs
+
+# sudo apt-get install -y libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1 && \
+# sudo apt-get install -y libswscale-dev libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libopenjp2-7-dev libavformat-dev libpq-dev && \
+# sudo apt-get install -y tesseract-ocr tesseract-ocr-eng tesseract-ocr-deu libtesseract-dev libleptonica-dev ldconfig libsm6 libxext6 python-opencv
