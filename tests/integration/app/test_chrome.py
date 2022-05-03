2143fe8a535bf56f685817d8c4082c4382870851
stehles <svenstehle@web.de>
2022-05-03 11:59:36 +0200

    extend localhost test

--- tests/integration/app/test_chrome.py
+++ tests/integration/app/test_chrome.py
@@ -3,6 +3,6 @@ from seleniumbase import BaseCase
 
 class ComponentsTest(BaseCase):
     """Basic test to debug and make sure we can display a webpage at all."""
-    def test_app_startup(self):
+    def test_chrome(self):
         self.open("http://www.google.com")
         self.assert_text("Google")
